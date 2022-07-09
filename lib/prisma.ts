80648ca1700eef24d431d04f0a5e5e711a802e1f
Afan Ahmed <aahmed@sandfield.co.nz>
2022-07-10 04:38:06 +1200

    Setup Prisma and connect with Heroku. Setup a basic Apollo Server. Created data Models with GraphQL Nexus and also setup basic GraphQL endpoints, types, context and resolvers.

---
+++ lib/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+// dont create new instance of Prisma client whenever Next.js fast refreshes
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+
+  prisma = global.prisma;
+}
+
+export default prisma;
